0d61d38a4ad854b19357f89862c34a17d7160783
gitcjh <1418503647@.qq.com>
2017-12-23 16:24:06 +0800

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="page-home">
+        <ui-header></ui-header>
+        <main class="page-body">
+            <div class="container">
+                这是首页内容
+                <router-link to="/about">关于</router-link>
+                <img src="/static/img/page.jpg" style="width: 100%">
+            </div>
+        </main>
+        <ui-footer>
+
+        </ui-footer>
+    </div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+            }
+        }
+    }
+</script>
+
+<style scoped>
+</style>
